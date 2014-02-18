ef28094b39c688d9212ff0d956da25413b792ef1
Brett Koonce <koonce@gmail.com>
2014-02-18 17:00:10 -0600

    python3 lol

--- src/runner.sh
+++ src/runner.sh
@@ -5,18 +5,18 @@
 echo 'Running TBModeller.py';
 echo 'Testing Single Sequence';
 
-python TBModeller.py T0644 MKFLKFSLLTAVLLSVVFAFSSCGDDDDTGYLPPSQAIQDALKKLYPNATAIKWEQKGVYYVADCQADGREKEVWFDANANWLMTETELNSINNLPPAVLTAFMESSYNNWVVDDVVILEYPNEPSTEFVVTVEQGKKVDLYFSEGGGLLHEKDVTNGDDTHWPRV;
+python3 TBModeller.py T0644 MKFLKFSLLTAVLLSVVFAFSSCGDDDDTGYLPPSQAIQDALKKLYPNATAIKWEQKGVYYVADCQADGREKEVWFDANANWLMTETELNSINNLPPAVLTAFMESSYNNWVVDDVVILEYPNEPSTEFVVTVEQGKKVDLYFSEGGGLLHEKDVTNGDDTHWPRV;
 
 echo 'Testing DR File';
 
-python TBModeller.py 'testdata/T0644.dr';
+python3 TBModeller.py 'testdata/T0644.dr';
 
 echo "Testing FASTA File"
 
-python TBModeller.py "testdata/T0644.fasta";
+python3 TBModeller.py "testdata/T0644.fasta";
 
 echo "Testing Multiple DR Files"
 
-python TBModeller.py "testdata/T0644.dr" "testdata/T0645.dr" "testdata/T0648.dr"
+python3 TBModeller.py "testdata/T0644.dr" "testdata/T0645.dr" "testdata/T0648.dr"
 
 echo 'Finished!'
